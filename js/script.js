b839441ab2d46f112eeb90100bb54589c1e5aa44
gorjnamron <gorjnamron@gmail.com>
2020-09-07 10:16:17 -0500

    this is a skeloton file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function play() {
+    let audio = document.getElementById("audio");
+    
+    audio.play();
+  }
+  function displayAudio() {
+    document.getElementById("hiddenMessage").play();
+}
